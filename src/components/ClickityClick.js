ac99817ec3158b87cca5482b1d05c77500b7916a
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-10-05 23:08:31 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,27 @@
-// Code ClickityClick Component Here
+import React from 'react';
+
+class ClickityClick extends React.Component {
+
+    state = {
+        clicked: false
+    }
+
+    clickHandler = () => {
+        this.setState(previousState => { 
+            return {
+                clicked: !previousState.clicked}
+        })
+    }
+
+    render() {
+        console.log("clicked``", this.state.clicked)
+        return (
+            <div>
+                <p>I have {this.state.clicked ? null : 'not'} been clicked!</p>
+                <button onClick={this.clickHandler}>Click me!</button>
+            </div>
+        );
+    }
+}
+
+export default ClickityClick;
